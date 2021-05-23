32a8f5850e6c1fd92d24f8633253f1d3143dadc1
Yarramsetty siva satyanarayana <64882708+siva6160@users.noreply.github.com>
2021-05-23 20:32:07 +0530

    Create even or odd num.java

---
+++ even or odd num.java
@@ -0,0 +1,18 @@
+package siva3;
+import java.util.*;
+public class vari3 {
+
+	public static void main(String[] args) {
+		int num;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a number ");
+		num=sc.nextInt();
+		if(num%2==0){
+			System.out.println("even number");
+		}
+		else{
+			System.out.println("odd number");
+		}
+	}
+
+}
